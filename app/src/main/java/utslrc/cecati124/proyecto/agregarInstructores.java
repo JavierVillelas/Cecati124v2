de7ddef4c03c6b2dd0d4cbab581279d1665a8282
JavierVillelas <November2011>
2017-06-22 04:36:31 -0700

    Login Funcionando - Javier Velazquez 22 junio

--- app/src/main/java/utslrc/cecati124/proyecto/agregarInstructores.java
+++ app/src/main/java/utslrc/cecati124/proyecto/agregarInstructores.java
@@ -4,17 +4,104 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class agregarInstructores extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class agregarInstructores extends AppCompatActivity{
+    Spinner spTipo;
+    Button boton;
+    EditText nombres, apellidos, telefono, direccion, correo, contrasena1, contrasena2;
+
+    ArrayAdapter<String> aaTipo;
+    String [] opcTipo = new String []{"Externo", "Plantilla", "Capacitacion"};
+    private RequestQueue requestQueue;
+    private static final String URL ="http://201.171.236.126:8080/modeloAndroid/guardarInstructor.php";
+    private StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_instructores);
+        spTipo=(Spinner) findViewById(R.id.spTipo);
+        nombres=(EditText) findViewById(R.id.etNombres);
+        apellidos=(EditText) findViewById(R.id.etApellidos);
+        telefono=(EditText) findViewById(R.id.etTelefono);
+        direccion=(EditText) findViewById(R.id.etDireccion);
+        correo=(EditText) findViewById(R.id.etCorreo);
+        contrasena1=(EditText) findViewById(R.id.etContrasena1);
+        contrasena2=(EditText) findViewById(R.id.etContrasena2);
+        boton=(Button) findViewById(R.id.btnIngresar);
+
+     //   spTipo.setOnItemSelectedListener(this);
+     //   aaTipo=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, opcTipo);
+     //   spTipo.setAdapter(aaTipo);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        boton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view) {
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject objeto = new JSONObject(response);
+                            if(objeto.names().get(0).equals("si")){
+                                Toast.makeText(getApplicationContext(), objeto.getString("si"), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),perfil.class));
+                            }else if(objeto.names().get(0).equals("existe")){
+                                Toast.makeText(getApplicationContext(), objeto.getString("no"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        //Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    protected Map<String,String> getParams() throws AuthFailureError {
+                        HashMap<String, String> hashMap = new HashMap <String, String>();
+                        hashMap.put("nombres", nombres.getText().toString());
+                        hashMap.put("apellidos", apellidos.getText().toString());
+                        hashMap.put("telefono", telefono.getText().toString());
+                        hashMap.put("direccion", direccion.getText().toString());
+                        hashMap.put("correo", correo.getText().toString());
+                        hashMap.put("contrasena1", contrasena1.getText().toString());
+                        hashMap.put("contrasena2", contrasena2.getText().toString());
+                        return hashMap;
+                    }
+                };
+                requestQueue.add(request);
+            }
+        });
     }
 
     public void perfil(View view){
         Intent intent = new Intent(this,perfil.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/utslrc/cecati124/proyecto/login.java
+++ app/src/main/java/utslrc/cecati124/proyecto/login.java
@@ -1,5 +1,6 @@
 package utslrc.cecati124.proyecto;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.SystemClock;
@@ -11,22 +12,84 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity implements View.OnClickListener{
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class login extends AppCompatActivity{
     Button boton;
-    EditText nombre;
+    EditText nombre, contrasena;
     ProgressBar barra;
+    private RequestQueue requestQueue;
+    private static final String URL ="http://201.171.236.126:8080/modeloAndroid/login.php";
+    private StringRequest request;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         boton=(Button) findViewById(R.id.btnIngresar);
         nombre=(EditText) findViewById(R.id.etCorreo);
+        contrasena=(EditText) findViewById(R.id.etPassword);
+        requestQueue = Volley.newRequestQueue(this);
 
         TextView recupera = (TextView) findViewById(R.id.link);
         recupera.setMovementMethod(LinkMovementMethod.getInstance());
 
-        anadirVistas();
+       // anadirVistas();
+        boton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view) {
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject objeto = new JSONObject(response);
+                            if(objeto.names().get(0).equals("si")){
+                                Toast.makeText(getApplicationContext(), objeto.getString("si"), Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),perfil.class));
+                            }else if(objeto.names().get(0).equals("no")){
+                                Toast.makeText(getApplicationContext(), objeto.getString("no"), Toast.LENGTH_SHORT).show();
+                            }else if(objeto.names().get(0).equals("falta")){
+                            Toast.makeText(getApplicationContext(), objeto.getString("falta"), Toast.LENGTH_SHORT).show();
+                        }
+
+
+                            
+                        } catch (JSONException e) {
+
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        //Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    protected  Map<String,String> getParams() throws AuthFailureError{
+                        HashMap<String, String> hashMap = new HashMap <String, String>();
+                        hashMap.put("correo", nombre.getText().toString());
+                        hashMap.put("contrasena", contrasena.getText().toString());
+
+                        return hashMap;
+                    }
+                };
+                requestQueue.add(request);
+            }
+        });
     }
 
     private void anadirVistas(){
@@ -41,17 +104,8 @@ public class login extends AppCompatActivity implements View.OnClickListener{
         //intent.putExtra("codigo",codigo1);
         startActivity(intent);
     }
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btnIngresar:
-             //   Toast.makeText(getApplicationContext(),"El usuario no puede ingresar al sistema",Toast.LENGTH_LONG).show();
-                new AsyncTask_load().execute();
-                break;
-            default:
-                break;
-        }
-    }
+
+
 
     public class AsyncTask_load extends AsyncTask<Void, Integer, Void>{
         int progreso;
